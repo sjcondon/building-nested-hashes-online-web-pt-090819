327e0b96a605dc725826b12c06dcb8f698497598
sarah <s.j.condon01@gmail.com>
2019-10-15 02:59:10 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,12 @@
 def second_challenge
 
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {},
       :matriarch => {},
